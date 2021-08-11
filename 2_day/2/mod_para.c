70030526a2ad4071b9326a5d59625112e155b426
anurag <mondal.anurag1410@gmail.com>
2021-08-11 17:39:45 +0100

    day2: q2: module parameter taking LKM done

---
+++ 2_day/2/mod_para.c
@@ -0,0 +1,26 @@
+#include <linux/module.h>
+#include <linux/moduleparam.h> // header file for module parameter
+#include <linux/init.h>
+
+static int value = 10; // initialize variable
+
+module_param(value,int,S_IRUGO); // taking variable as command line argument
+
+static int my_mod_para_init(void) // initialize module function
+{
+	printk("welcome to my module.\n");
+	printk("the value in module parameter: %d\n",value); // printing of command line argument
+	return 0;
+}
+
+static void my_mod_para_exit(void) // exit module function
+{
+	printk("good bye\n");
+}
+
+module_init(my_mod_para_init);
+module_exit(my_mod_para_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("ANURAG");
+MODULE_DESCRIPTION("day2: q2: parameter input linux kernel module");
