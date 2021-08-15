93cbf0629eb3361df9a79534457ca67f95fe5fea
anurag <mondal.anurag1410@gmail.com>
2021-08-15 13:21:40 +0100

    day4: q1: modification done now device name also display that driver expect and comment the programs

--- 4_day/1/app.c
+++ 4_day/1/app.c
@@ -2,19 +2,17 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <unistd.h>
 
 int main()
 {
 	int fd;
-	fd = open("/dev/MyCharDevice",O_RDWR,777);
-	
-
-	if(fd < 0)
+	fd = open("/dev/MyCharDevice",O_RDWR,777); // open system call with flag and permission
+	if(fd < 0) //condition to check whether open successfully done or not
 	{
 		printf("not able to open the device\n");
 		return -1;
 	}
-	
-	close(fd);
+	close(fd); // close system call
 	return 0;
 }
--- 4_day/1/mydriver.c
+++ 4_day/1/mydriver.c
@@ -1,3 +1,7 @@
+/*Write a character driver with open and close functionality. 
+ * Test the driver by writing an application that opens and closes the device driver. 
+ * When open or closed, the open and close calls in the driver should be executed.*/
+
 #include <linux/cdev.h>
 #include <linux/fs.h>
 #include <linux/kdev_t.h>
@@ -7,24 +11,24 @@
 #include <linux/uaccess.h>
 
 
-
+/*declaration of functions*/
 int my_open_fn(struct inode *inode, struct file *file_d);
 int my_release_fn(struct inode *inode,struct file *file_d);
 
-
+/*the file operation structure which device have to perform*/
 struct file_operations op ={
 	.owner = THIS_MODULE,
 	.open = my_open_fn,
 	.release = my_release_fn,
 };
-
+/*open function defination*/
 int my_open_fn(struct inode *inode, struct file *file_d)
 {
 	printk("My device is using My charcter driver\n");
 	return 0;
 }
 
-
+/*close function defination*/
 int my_release_fn(struct inode *inode, struct file *file_d)
 {
 	printk("my device in now logging off\n");
@@ -32,37 +36,37 @@ int my_release_fn(struct inode *inode, struct file *file_d)
 }
 
 
-
-struct cdev *MyCharDriver;
+/////////////////////////////////////////////////////////////////////////////////
+struct cdev *MyCharDevice;
 
 dev_t my_driver_no;
-
+////////////////////////////////////////////////////////////////////module initialization function
 static int my_driver_mod_init(void)
 {
 //	dev_t my_driver_no;
 	int major,minor;
-	my_driver_no = MKDEV(255,0);
-	major = MAJOR(my_driver_no);
-	minor = MINOR(my_driver_no);
+	my_driver_no = MKDEV(255,0); // making the device number
+	major = MAJOR(my_driver_no); // finding the major number
+	minor = MINOR(my_driver_no); // macro for finding the minor number
 	
 	printk("module logging in\n");
 
-	int result = register_chrdev_region(my_driver_no,1,"MyCharDriver");
+	int result = register_chrdev_region(my_driver_no,1,"MyCharDriver"); //statically making the device driver
 
-	if(result < 0)
+	if(result < 0) // condition to check whether registration of done or not
 	{
 		printk("\nDriver not allocated with driver number\n");
 		return -1;
 	}
 
 	printk("my charcter driver got the region with major no %d and minor no %d\n",major,minor);
+	printk("\nFor open and write operation the driver expect the flloing device:\nMyCharDevice\n");
+	MyCharDevice = cdev_alloc(); // allocation of the my cal device
+	MyCharDevice->ops = &op;
 
-	MyCharDriver = cdev_alloc();
-	MyCharDriver->ops = &op;
+	result = cdev_add(MyCharDevice,my_driver_no,1); //add the character device by kernel
 
-	result = cdev_add(MyCharDriver,my_driver_no,1);
-
-	if(result < 0)
+	if(result < 0) // condition to check whether device is add or not
 	{
 		printk("Driver not recognised by kernel\n");
 		unregister_chrdev_region(my_driver_no,1);
@@ -71,13 +75,13 @@ static int my_driver_mod_init(void)
 
 	return 0;
 }
-
+////////////////////////////////////////////////////////////////////module exit function
 static void my_driver_mod_exit(void)
 {
 	printk("module going to be logging off\n");
 	unregister_chrdev_region(my_driver_no,1);
 	printk("Driver now unregister\n");
-	cdev_del(MyCharDriver);
+	cdev_del(MyCharDevice);
 	printk("kernel forgot my chracter driver\n");
 }
 
